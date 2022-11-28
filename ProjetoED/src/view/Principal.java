2a01c6c9761be8fe8ccbb328f0a12f3af17925b7
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-11-28 00:18:55 -0300

    Update Principal.java

--- ProjetoED/src/view/Principal.java
+++ ProjetoED/src/view/Principal.java
@@ -1,16 +1,8 @@
 package view;
 
-import controller.ModificacaoCadastroController;
-
 public class Principal {
 
 	public static void main(String[] args) throws Exception{
-		String caminho = "C:\\TEMP";
-		String arquivo = "cadastro.csv";
-		ModificacaoCadastroController mod = new ModificacaoCadastroController();
-		mod.novoCadastro(caminho, arquivo, 41, 60, 8000.0);
-		mod.novoCadastro(caminho, arquivo, 31, 40, 5000.0);
-		mod.novoCadastro(caminho, arquivo, 21, 30, 3000.0);
 		
 	}
 }
