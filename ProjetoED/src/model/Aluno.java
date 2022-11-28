ac882e3a044611ac4a214ed0b8d558bae05b9dcd
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-11-28 00:16:20 -0300

    Add files via upload

---
+++ ProjetoED/src/model/Aluno.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Aluno {
+	
+	public String usuario;
+	public String curso;
+	public String email;
+	public String semestre;
+	public String turno;
+	public String senha;
+	public double RA;
+}
---
+++ ProjetoED/src/model/Professor.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.util.Date;
+
+public class Professor {
+	
+	public String usuario;
+	public String curso;
+	public String email;
+	public String especialidade;
+	public Date horaInicioDisp;
+	public Date horaFimDisp;
+	public String senha;
+	public String exp;
+	public boolean coordenador;
+}
