b159a465637b2d0629f893296568596020396ae1
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-11-28 23:46:11 -0300

    Add files via upload

---
+++ ProjetoED/src/model/GrupoTCC.java
@@ -0,0 +1,10 @@
+package model;
+
+public class GrupoTCC {
+	
+	public String nome;
+	public String modalidade;
+	public String Professor;
+	public String[] nomeIntegrantes;
+	public String tema;
+}
