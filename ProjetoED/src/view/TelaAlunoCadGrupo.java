758ab66a07629a5e8422003697435d39efa4d110
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-11-30 02:52:44 -0300

    Update TelaAlunoCadGrupo.java

--- ProjetoED/src/view/TelaAlunoCadGrupo.java
+++ ProjetoED/src/view/TelaAlunoCadGrupo.java
@@ -1,4 +1,7 @@
-package view;
+package panel;
+
+import model.Aluno;
+import model.GrupoTCC;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
@@ -10,13 +13,16 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
-import enumeracoes.Fonte;
+import panel.enumeracoes.Fonte;
+import javax.swing.JList;
 
 public class TelaAlunoCadGrupo extends JFrame {
 
@@ -25,97 +31,114 @@ public class TelaAlunoCadGrupo extends JFrame {
 	private JTextField tfNomeGrupo;
 	private JComboBox<String> comboModalidadeGrupo;
 	private JComboBox<String> comboOrientador;
-	private JTextField textField_1;
+	private JTextField tfNomeIntegrantes;
 	private JTextField tfTemaGrupo;
 
 	public TelaAlunoCadGrupo() {
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setBounds(100, 100, 554, 407);
 		setResizable(false);
-		
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
+
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(74, 74, 74, 125));
 		panel_1.setBounds(0, 0, 602, 368);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		
+
 		panelModal = new JPanel();
 		panelModal.setBounds(92, 11, 349, 346);
 		panelModal.setBackground(new Color(255, 255, 255));
 		panel_1.add(panelModal);
 		panelModal.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("Crie seu grupo de TCC");
 		lblNewLabel.setForeground(Color.RED);
 		lblNewLabel.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 16));
 		lblNewLabel.setBounds(87, 11, 190, 20);
 		panelModal.add(lblNewLabel);
-		
+
 		JLabel lblNomeGrupo = new JLabel("Nome do grupo");
 		lblNomeGrupo.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 12));
 		lblNomeGrupo.setBounds(10, 36, 86, 14);
 		lblNomeGrupo.setAlignmentX(2.0f);
 		panelModal.add(lblNomeGrupo);
-		
+
 		tfNomeGrupo = new JTextField();
 		tfNomeGrupo.setBounds(10, 55, 329, 20);
 		panelModal.add(tfNomeGrupo);
 		tfNomeGrupo.setColumns(10);
-		
+
 		JLabel lblModalidadeGrupo = new JLabel("Modalidades do projeto");
 		lblModalidadeGrupo.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 12));
 		lblModalidadeGrupo.setBounds(10, 87, 158, 14);
 		panelModal.add(lblModalidadeGrupo);
-		
+
 		comboModalidadeGrupo = new JComboBox<>();
-		comboModalidadeGrupo.setModel(new DefaultComboBoxModel<>(new String[] {"Software", "Artigo", "Monografia"}));
+		comboModalidadeGrupo.setModel(new DefaultComboBoxModel<>(
+				new String[] { "=========== Escolha sua Modalidade ===========", "Software", "Artigo", "Monografia" }));
 		comboModalidadeGrupo.setBounds(10, 103, 329, 20);
 		panelModal.add(comboModalidadeGrupo);
-		
+
 		comboOrientador = new JComboBox<>();
+		comboOrientador.setModel(new DefaultComboBoxModel<>(new String [] { "============ Escolha seu Orientador ===========", "Leandro Colevati", "Cristina Correa", "Wellington Pinto" }));
 		comboOrientador.setBounds(10, 150, 329, 20);
 		panelModal.add(comboOrientador);
-		
+
 		JLabel lblProfessorOrientador = new JLabel("Professor Orientador");
 		lblProfessorOrientador.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 12));
 		lblProfessorOrientador.setBounds(10, 134, 158, 14);
 		panelModal.add(lblProfessorOrientador);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(10, 197, 329, 20);
-		panelModal.add(textField_1);
-		
+
+		tfNomeIntegrantes = new JTextField();
+		tfNomeIntegrantes.setColumns(10);
+		tfNomeIntegrantes.setBounds(10, 197, 329, 20);
+		panelModal.add(tfNomeIntegrantes);
+
 		JLabel lblModalidadeGrupo_1_1 = new JLabel("Nome dos Integrantes");
 		lblModalidadeGrupo_1_1.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 12));
 		lblModalidadeGrupo_1_1.setBounds(10, 181, 158, 14);
 		panelModal.add(lblModalidadeGrupo_1_1);
-		
+
 		tfTemaGrupo = new JTextField();
 		tfTemaGrupo.setColumns(10);
 		tfTemaGrupo.setBounds(10, 264, 329, 20);
 		panelModal.add(tfTemaGrupo);
-		
+
 		JLabel lblModalidadeGrupo_1_1_1 = new JLabel("Tema do projeto");
 		lblModalidadeGrupo_1_1_1.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 12));
 		lblModalidadeGrupo_1_1_1.setBounds(10, 248, 158, 14);
 		panelModal.add(lblModalidadeGrupo_1_1_1);
-		
+
 		JButton btnCancelarGrupo = new JButton("CANCELAR");
+		btnCancelarGrupo.addActionListener((ActionEvent e) -> {
+			TelaAluno aluno = new TelaAluno();
+			aluno.setVisible(true);
+			setVisible(false);
+		});
 		btnCancelarGrupo.setBounds(34, 312, 118, 23);
 		panelModal.add(btnCancelarGrupo);
-		
+
 		JButton btnSalvarGrupo = new JButton("SALVAR");
 		btnSalvarGrupo.addActionListener((ActionEvent e) -> {
-			TelaAlunoGrupo grupo = new TelaAlunoGrupo();
-			grupo.setVisible(true);
-			setVisible(false);
+			if (tfNomeGrupo.getText().isEmpty()
+					|| comboModalidadeGrupo.getSelectedItem() == "=========== Escolha sua Modalidade ===========" || 
+					comboOrientador.getSelectedItem() == "=========== Escolha seu Orientador ===========") {
+				JOptionPane.showMessageDialog(null, "Por favor, preencha todos os campos de cadastro do TCC");
+			} else {
+				GrupoTCC grupoTCC = new GrupoTCC();
+				grupoTCC.nome = tfNomeGrupo.getText();
+				grupoTCC.modalidade = (String) comboModalidadeGrupo.getSelectedItem();
+				grupoTCC.Professor = (String) comboOrientador.getSelectedItem();
+				TelaAlunoGrupo grupo = new TelaAlunoGrupo();
+				grupo.setVisible(true);
+				setVisible(false);
+			}
 		});
 		btnSalvarGrupo.setBounds(186, 312, 118, 23);
 		panelModal.add(btnSalvarGrupo);
@@ -125,20 +148,20 @@ public class TelaAlunoCadGrupo extends JFrame {
 		lblSemGrupo.setFont(new Font(Fonte.ARIAL.getFonte(), Font.PLAIN, 18));
 		lblSemGrupo.setBounds(135, 67, 212, 31);
 		contentPane.add(lblSemGrupo);
-		
+
 		JButton btnNewButton = new JButton("Clique aqui e crie seu grupo");
 		btnNewButton.setEnabled(false);
 		btnNewButton.setFont(new Font(Fonte.ARIAL.getFonte(), Font.BOLD, 12));
 		btnNewButton.setBounds(43, 107, 423, 134);
 		contentPane.add(btnNewButton);
-		
+
 		JPanel panel = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
 		flowLayout.setAlignment(FlowLayout.LEFT);
 		panel.setBackground(new Color(187, 187, 187));
 		panel.setBounds(0, 0, 602, 38);
 		contentPane.add(panel);
-		
+
 		JLabel lblIntroducao = new JLabel(" Introdu\u00E7\u00E3o");
 		lblIntroducao.setHorizontalAlignment(SwingConstants.LEFT);
 		lblIntroducao.setVerticalAlignment(SwingConstants.TOP);
